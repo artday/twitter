bb325a10ddcdd62ee6a6c62f2e80b4792853b9bf
artday <admin@artday.com.ua>
2020-06-26 12:02:27 +0300

    updated

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,8 @@ class HomeController extends Controller
      */
     public function index(Request $request)
     {
+        dd($request->user()->tweetsFromFollowing);
+
         return view('home');
     }
 }
