3d0b05f92ddfff3621bc9a31fbf1ecabbd6efb2b
artday <admin@artday.com.ua>
2020-06-29 08:10:52 +0300

    created_add field added to resource output

--- app/Http/Resources/TweetResource.php
+++ app/Http/Resources/TweetResource.php
@@ -6,18 +6,14 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class TweetResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
+
     public function toArray($request)
     {
         return [
             'id' => $this->id,
             'body' => $this->body,
             'user' => new UserResource($this->user),
+            'created_at' => $this->created_at->timestamp,
         ];
     }
 }
