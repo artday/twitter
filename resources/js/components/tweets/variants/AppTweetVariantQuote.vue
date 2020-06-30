0bb371fb18e565f51ed86eee7df3ca4901e3a145
artday <admin@artday.com.ua>
2020-06-30 10:00:06 +0300

    'quote'variant of tweet component created

---
+++ resources/js/components/tweets/variants/AppTweetVariantQuote.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="flex w-full">
+        <div class="mr-3">
+            <img :src="tweet.user.avatar" alt="" class="w-12 rounded-full">
+        </div>
+        <div class="flex-grow">
+            <app-tweet-username :user="tweet.user" />
+            <p class="text-gray-300 whitespace-pre-wrap">{{ tweet.body }}</p>
+
+            <app-tweet
+                class="border rounded-lg mt-4"
+                :tweet="tweet.original_tweet"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            tweet: {
+                required: true,
+                type: Object
+            }
+        }
+    }
+</script>
