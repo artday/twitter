478c0b8810e82280a6a17034500437ac99d2dc78
artday <admin@artday.com.ua>
2020-07-01 21:33:03 +0300

    added computed 'liked' property to determine if the current user liked the tweet

--- resources/js/components/tweets/actions/AppTweetLikeAction.vue
+++ resources/js/components/tweets/actions/AppTweetLikeAction.vue
@@ -2,6 +2,7 @@
     <a href="" class="flex items-center text base">
         <svg xmlns="http://www.w3.org/2000/svg"
              class="fill-current text-gray-600 w-5 mr-2"
+             :class="{'text-red-600': liked}"
              viewBox="0 0 24 24" width="24" height="24">
             <path d="M12.76 3.76a6 6 0 0 1 8.48
                             8.48l-8.53 8.54a1 1 0 0 1-1.42 0l-8.53-8.54a6 6 0 0 1
@@ -9,16 +10,30 @@
                             0-5.66-5.66l-1.46 1.47a1 1 0 0 1-1.42
                             0L9.83 5.17a4 4 0 1 0-5.66 5.66L12 18.66l7.83-7.83z"/>
         </svg>
-        <span class="text-gray-600">{{ tweet.likes_count }}</span>
+        <span class="text-gray-600" :class="{'text-red-600': liked}" >
+            {{ tweet.likes_count }}
+        </span>
     </a>
 </template>
 
 <script>
+    import { mapGetters } from 'vuex'
+
     export default {
         props: {
             tweet: {
                 required: true,
                 type: Object
+            }
+        },
+
+        computed: {
+            ...mapGetters({
+                likes: 'likes/likes'
+            }),
+
+            liked () {
+                return this.likes.includes(this.tweet.id)
             }
         }
     }
